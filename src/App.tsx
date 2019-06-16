7ce01f792ab706d30a4e0d62d272e95c74cd74e9
Ingmar Dasseville <ingmar.dasseville@cs.kuleuven.be>
2019-06-16 13:36:23 +0200

    connecteren mogelijk, waitscreen activeerbaar

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Question from './question/Question'
 import {QuestionC} from "./question/QuestionC";
 import {Container} from "@material-ui/core";
+import {WaitScreen} from "./Waiting/WaitScreen";
 
-const App: React.FC = () => {
-    const question = new Question("Wie?", ["A", "B", "C"]);
-    const comp = new QuestionC(question);
+interface AppState {
+    socket: WebSocket
+    currentWindow: Component
+}
 
-    const bumperStyle = {
-        height: "50px"
-    };
+class App extends Component<{}, AppState> {
+    constructor(props: {}) {
+        super(props);
 
-    return <Container maxWidth="md">
-        <div style={bumperStyle}/>
-        {comp.render()}
-    </Container>
+        const question = new Question("Wie?", ["A", "B", "C"]);
+        const comp = new QuestionC(question);
 
-};
+        this.state = {
+            socket: new WebSocket("ws://localhost:7070/play"),
+            currentWindow: comp
+        };
+        this.initialiseApp()
+    }
+
+    render() {
+        const bumperStyle = {
+            height: "50px"
+        };
+
+        return <Container maxWidth="md">
+            <div style={bumperStyle}/>
+            {this.state.currentWindow.render()}
+        </Container>
+    }
+
+
+    private initialiseApp() {
+        const ws = this.state.socket
+        ws.onmessage = (evt) => {
+            const data = evt.data
+            if (!data.q) {
+                const newState: AppState = {socket: ws, currentWindow: new WaitScreen({})};
+                this.setState(newState)
+
+            }
+        };
+    }
+
+}
 
 export default App;
---
+++ src/Waiting/WaitScreen.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+export class WaitScreen extends React.Component<{}, {}> {
+
+    render() {
+        return <div>Kijk verder naar de show</div>;
+    }
+}
