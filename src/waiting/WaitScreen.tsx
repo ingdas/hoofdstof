f0bddb29bf46e9234e930ada2793fcade401301d
Ingmar Dasseville <ingmar.dasseville@cs.kuleuven.be>
2019-07-13 20:12:11 +0200

    compact wait screen

--- src/waiting/WaitScreen.tsx
+++ src/waiting/WaitScreen.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 
 
-export class WaitScreen extends React.Component<{}, {}> {
-
-    render() {
-        return <div>Kijk verder naar de show</div>;
-    }
-}
+export const WaitScreen = (_: {}) => (
+    <div>Kijk verder naar de show</div>
+);
