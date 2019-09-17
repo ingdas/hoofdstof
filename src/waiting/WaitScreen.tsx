2481c3d3963f761754abfa0ba8eaa3a307d3971a
Ingmar Dasseville <ingdas@gmail.com>
2019-09-17 13:26:31 +0200

    Background image logo

--- src/waiting/WaitScreen.tsx
+++ src/waiting/WaitScreen.tsx
@@ -1,6 +1,15 @@
 import React from "react";
+import Background from '../img/logo.png';
 
 
-export const WaitScreen = (_: {}) => (
-    <div>Kijk verder naar de show</div>
-);
+export const WaitScreen = (_: {}) => {
+    const logobg = {
+        backgroundImage: `url(${Background})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'contain',
+        height: 'calc(100% - 100px)',
+        width: '100%'
+
+    };
+    return <div style={logobg}>Kijk verder naar de show</div>;
+};
